e41ad849e349c71838ab8ff6388833e8a9050df2
svetlana <>
2024-11-23 21:57:11 +0300

    feat: add PUT req & edited action, add singleton-approach to socket, concurrently

---
+++ src/socket.ts
@@ -0,0 +1,10 @@
+import { Socket, io } from 'socket.io-client'
+
+let socket: Socket | null = null
+
+export const getSocket = (): Socket => {
+  if (!socket) {
+    socket = io('http://localhost:3003')
+  }
+  return socket
+}
